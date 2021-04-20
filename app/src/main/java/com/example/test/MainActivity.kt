0bb6105d6774411fb4307bd48d6c51a750680459
Yaroslav <Yaroslav_mi@mail.ru>
2021-04-20 10:07:02 +0300

    android

---
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.test
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import java.util.jar.Attributes
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.test)
+        // findViewById<класс объекта>(id нашего объекта)
+        val name = findViewById<Button>(R.id.button)
+
+
+    }
+
+    fun click(view: View){
+        // context, 3 переменная - длительность отображения
+        val mes = Intent(this,MainTestActivity::class.java)
+        startActivity(mes)
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        }
+
+    }
